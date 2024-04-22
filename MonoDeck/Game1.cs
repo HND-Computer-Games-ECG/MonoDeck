2cfcb44e2f643b6c8067a2ec2cbf7272e6422b34
Nigel Kennington <nigel.kennington@edinburghcollege.ac.uk>
2024-04-22 22:02:34 +0100

    Commenting

--- MonoDeck/Game1.cs
+++ MonoDeck/Game1.cs
@@ -16,10 +16,12 @@ namespace MonoDeck
 
         private MouseState ms_curr, ms_old;
 
+        // Variables for data management
         List<Texture2D> _allCardBacks;
         List<Texture2D> _allCardFaces;
         List<CardData> _allCardData;
 
+        // Actual game world stuff
         private Deck _testDeck;
         private Hand _playerHand;
         private List<Character> _weePeeps;
@@ -33,6 +35,7 @@ namespace MonoDeck
 
         protected override void Initialize()
         {
+            // Set up the card info
             _allCardData = new List<CardData>
             {
                 new (CardType.Club, CardColour.Black, CardRank.Royal, 1),
@@ -57,7 +60,10 @@ namespace MonoDeck
                 new (CardType.Spade, CardColour.Black, CardRank.Royal, 4),
             };
 
+            // Create a "hand" structure to hold player's cards
             _playerHand = new Hand(new Vector2(_graphics.PreferredBackBufferWidth/2 - 70, _graphics.PreferredBackBufferHeight - 75), 7);
+
+            // List to hold game characters
             _weePeeps = new List<Character>();
 
             base.Initialize();
@@ -67,6 +73,7 @@ namespace MonoDeck
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            // Preload all the art into lists (this would be better in a spritesheet, but this is easier...)
             _allCardBacks = new List<Texture2D>
             {
                 Content.Load<Texture2D>("Cards/Backs/cardBack_blue1"),
@@ -109,13 +116,20 @@ namespace MonoDeck
                 Content.Load<Texture2D>("Cards/Faces/cardSpadesK"),
             };
 
+            // The card faces need to match up with the card data set up in Initialise or this won't work - so let's check for that
             Debug.Assert(_allCardFaces.Count == _allCardData.Count, "Card face count does not match card data count");
 
+            // Create a deck structure to hold all the cards - pick a random card back for it too.
             _testDeck = new Deck(_allCardBacks[RNG.Next(_allCardBacks.Count)], new Vector2(_graphics.PreferredBackBufferWidth - 250, 25));
+
+            // Add one of each card to the deck
             for (var i = 0; i < _allCardData.Count; i++)
                 _testDeck.AddCard(_allCardFaces[i], _allCardData[i]);
+
+            // Shuffle the deck
             _testDeck.Shuffle();
 
+            // Create the peeps and add them to the list for easy management
             _weePeeps.Add(new Character(Content.Load<Texture2D>("charsheet_chroma"), Content.Load<Texture2D>("charsheet_overlay"), new Vector2(50, 80), new Point(3, 2), Color.Red));
             _weePeeps.Add(new Character(Content.Load<Texture2D>("charsheet_chroma"), Content.Load<Texture2D>("charsheet_overlay"), new Vector2(330, 20), new Point(3, 2), Color.DimGray));
             _weePeeps.Add(new Character(Content.Load<Texture2D>("charsheet_chroma"), Content.Load<Texture2D>("charsheet_overlay"), new Vector2(200, 170), new Point(3, 2), Color.DodgerBlue));
@@ -123,13 +137,17 @@ namespace MonoDeck
 
         protected override void Update(GameTime gameTime)
         {
+            // Make a local variable so we don't have to time gametime.Elapsed...blahblahblah each time
             var dT = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Get what the mouse is doing
             ms_curr = Mouse.GetState();
             
+            // Update the deck and hand
             _testDeck.Update(dT, ms_curr.Position);
             _playerHand.Update(dT, ms_curr.Position);
 
-
+            // Update the peeps
             foreach (var peep in _weePeeps)
                 peep.Update(dT, ms_curr.Position);
 
@@ -143,10 +161,11 @@ namespace MonoDeck
 
                 if (_playerHand.SelectedCard != -1)
                     _testDeck.DiscardCard(_playerHand.PullCard());
-
             }
 
+            // Store what the mouse WAS doing
             ms_old = ms_curr;
+
             base.Update(gameTime);
         }
 
@@ -155,6 +174,7 @@ namespace MonoDeck
             GraphicsDevice.Clear(Color.DarkGreen);
 
             _spriteBatch.Begin();
+
             _testDeck.Draw(_spriteBatch);
 
             foreach (var peep in _weePeeps)
